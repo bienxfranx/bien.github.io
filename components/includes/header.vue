fe51781f887278baa2fc916a5d64a253a44fccf0
bien francis <bienfrancisp@gmail.com>
2022-12-11 20:56:52 +0800

    first

---
+++ components/includes/header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="navigation">
+        <div class="logo">
+            <p>B</p>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>ads</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: 'default'
+}
+</script>
